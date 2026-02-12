f9a465b209eac827c626477f884c67b153f1bc9d
kaye-teeshoppen <kag@teeshoppen.dk>
2026-02-13 00:13:55 +0800

    fix(migration): replace invalid STRING type with VARCHAR for sku column

--- src/migrations/11-tee-order-line-items-sku.cjs
+++ src/migrations/11-tee-order-line-items-sku.cjs
@@ -1,16 +1,14 @@
 module.exports = {
   up: async ({ context: queryInterface }) => {
-    await queryInterface.sequelize
-      .query('BEGIN;')
+    await queryInterface.sequelize.query('BEGIN;')
       .then(() =>
         queryInterface.sequelize.query(
-          'ALTER TABLE "orders"."order_line_items" ADD COLUMN IF NOT EXISTS "sku" STRING;'
+          'ALTER TABLE "orders"."order_line_items" ADD COLUMN IF NOT EXISTS "sku" VARCHAR(255);'
         )
       )
       .then(() => queryInterface.sequelize.query('COMMIT;'))
       .catch(async (err) => {
         await queryInterface.sequelize.query('ROLLBACK;');
-        // eslint-disable-next-line no-console
         console.log(err);
         throw err;
       });
